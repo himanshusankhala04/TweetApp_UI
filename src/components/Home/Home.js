9259f0edd58eb1ef2797e241afe67a370c8060d5
unknown <cogdotnet1019@iiht.tech>
2022-08-31 23:12:57 +0530

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import Tweet from '../Tweet/Tweet';
+import axios from "axios";
+import './Home.css';
+
+import TweetAppService from '../../utilities/TweetAppService';
+export function Home() {
+  const service = new TweetAppService();
+
+
+  const [tweetdata, setTweetdata] = useState([]);
+  const [updateLike, setUpdateLike] = useState();
+  const [refreshKey, setRefreshKey] = useState(0);
+  const url = "https://tweetapp120220831221357.azurewebsites.net";
+  useEffect(() => {
+      async function fetchTweets() {
+      const response = await axios.get(`${url}/api/v1.0/tweets/all`, {
+        "method": "GET",
+        "headers": {
+          "Accept": "application/json"
+        }
+      });
+    setTweetdata(response.data);
+    }
+    fetchTweets();
+  }, [refreshKey]);
+    
+  
+  function refreshUpdate(){
+   
+    setTimeout(()=>{
+          
+      setRefreshKey(old => old+1);
+      },500);
+    
+  }
+  return (
+    <div className="heading">
+      <div className="comp" >
+        <div className="bg">
+        {[...tweetdata].reverse().map((dataPoint, index) => (
+            <Tweet dataPoint={dataPoint} key={index} refreshUpdate={refreshUpdate} />
+            
+        ))}
+        </div>
+      </div>
+  
+    </div>
+  )
+}
